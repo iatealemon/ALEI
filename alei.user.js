a79c34dd18575f16190637ac7ec98e9ffa577b3b
Molisson <molisson@tutamail.com>
2024-08-14 12:13:18 +0200

    Fix download, icon and update URL
    Previously the URLs did not have `/ALEI/` in there, it was more of a copy and paste issue I did

--- alei.user.js
+++ alei.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name ALE Improvements
 // @description Changes to make ALE better.
-// @version 19.6
+// @version 19.7
 // @author molis, mici1234, wanted2001, gcp5o
 // @match *://www.plazmaburst2.com/level_editor/map_edit.php*
 // @connect github.com
 // @connect githubusercontent.com
-// @downloadURL https://github.com/Molisson/raw/main/alei.user.js
+// @downloadURL https://github.com/Molisson/ALEI/raw/main/alei.user.js
 // @grant GM.xmlHttpRequest
-// @icon https://github.com/Molisson/blob/main/icon.png?raw=true
+// @icon https://github.com/Molisson/ALEI/blob/main/icon.png?raw=true
 // @run-at document-start
-// @updateURL https://github.com/Molisson/raw/main/alei.user.js
+// @updateURL https://github.com/Molisson/ALEI/raw/main/alei.user.js
 // ==/UserScript==
 
 /******/ (() => { // webpackBootstrap
