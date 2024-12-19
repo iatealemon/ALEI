8d29ff408d02d37f5fd1f0d8bf91a0b92d8f727a
iatealemon <84639864+iatealemon@users.noreply.github.com>
2024-12-20 00:46:44 +0200

    Moved SelectedObjects

--- src/entity/entity.js
+++ src/entity/entity.js
@@ -2,9 +2,57 @@ import { addEntitiesToUIDMap } from "./uidmap.js";
 import { addEntitiesToParameterMap } from "./parametermap.js";
 import { ocmHandleEntitiesCreation } from "../ocm/ocm.js";
 import { aleiSettings } from "../storage/settings.js";
+import { aleiLog, logLevel } from "../log.js";
 
 export function onEntitiesCreated(newEntities) {
     addEntitiesToUIDMap(newEntities);
     addEntitiesToParameterMap(newEntities);
     if (aleiSettings.ocmEnabled) ocmHandleEntitiesCreation(newEntities);
+}
+
+export let SelectedObjects = [];
+unsafeWindow.getSelection = () => { return SelectedObjects; };
+
+export function clearSelectedObjects() {
+    SelectedObjects = [];
+}
+
+export function patchEntityClass() {
+    let og_E = unsafeWindow.E;
+    unsafeWindow.E = function(_class) {
+        let result = new og_E(_class);
+
+        // Adding property.
+        if(_class == "water") result.pm.attach = -1;
+        else if(_class == "decor") result.pm.text = "Hello World!";
+        else if(_class == "trigger") {
+            let entries = Object.entries(result.pm);
+            entries.splice(5, 0, ["execute", false]);
+            result.pm = Object.fromEntries(entries);
+        }
+        else if(_class == "region") result.pm.uses_timer = false;
+
+        result.fixPos = function() {}; // For proper snapping.
+
+        result.Remove = () => { // Better be safe...
+            NewNote(`ALEI: This absolutely should not happen, please report to ALEI developers, error: E.Remove got called when it shouldn't be`, `#FF0000`);
+            debugger;
+        };
+
+        let proxy = new Proxy(result, {
+            set: function(target, key, value, receiver) {
+                if (key == "selected") {
+                    const oldValue = target.selected;
+                    if ((value && !oldValue)) {
+                        SelectedObjects.push(receiver);
+                    } else if (!value && oldValue) {
+                        SelectedObjects.splice(SelectedObjects.indexOf(receiver), 1);
+                    }
+                }
+                return Reflect.set(target, key, value, receiver);
+            }
+        });
+        return proxy;
+    }
+    aleiLog(logLevel.DEBUG, "Patched entity.");
 }
--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ import { makeCommentBox, setCurrentCommentedTrigger, setCommentsResizeObserverTa
 
 import "./draggablewindow/draggablewindow.js";
 
-import { onEntitiesCreated } from "./entity/entity.js";
+import { onEntitiesCreated, patchEntityClass, SelectedObjects, clearSelectedObjects } from "./entity/entity.js";
 import { patchUpdatePhysicalParam, updateParameters, REGION_EXECUTE_PARAM_ID } from "./entity/parameter.js";
 import { loadParameterMap, parameterMapHandleParametersRemoval, clearParameterMap } from "./entity/parametermap.js";
 import { replaceParamValueUID } from "./entity/parameterutils.js";
@@ -2324,48 +2324,6 @@ function patch_m_down() {
     }
 }
 
-window.SelectedObjects = [];
-
-function patchEntityClass() {
-    let og_E = E;
-    window.E = function(_class) {
-        let result = new og_E(_class);
-
-        // Adding property.
-        if(_class == "water") result.pm.attach = -1;
-        else if(_class == "decor") result.pm.text = "Hello World!";
-        else if(_class == "trigger") {
-            let entries = Object.entries(result.pm);
-            entries.splice(5, 0, ["execute", false]);
-            result.pm = Object.fromEntries(entries);
-        }
-        else if(_class == "region") result.pm.uses_timer = false;
-
-        result.fixPos = function() {}; // For proper snapping.
-
-        result.Remove = () => { // Better be safe...
-            NewNote(`ALEI: This absolutely should not happen, please report to ALEI developers, error: E.Remove got called when it shouldn't be`, `#FF0000`);
-            debugger;
-        };
-
-        let proxy = new Proxy(result, {
-            set: function(target, key, value, receiver) {
-                if (key == "selected") {
-                    const oldValue = target.selected;
-                    if ((value && !oldValue)) {
-                        SelectedObjects.push(receiver);
-                    } else if (!value && oldValue) {
-                        SelectedObjects.splice(SelectedObjects.indexOf(receiver), 1);
-                    }
-                }
-                return Reflect.set(target, key, value, receiver);
-            }
-        });
-        return proxy;
-    }
-    aleiLog(logLevel.DEBUG, "Patched entity.");
-}
-
 function PasteFromClipBoard(ClipName) {
     var clipboard = new Object();
     if (sessionStorage[ClipName] == undefined) {
@@ -2595,7 +2553,7 @@ function ServerRequest_handleMapData(mapCode) {
 
 function handleServerRequestResponse(request, operation, response) {
     if (response.indexOf("var es = new Array();") != -1) {
-        window.SelectedObjects = [];
+        clearSelectedObjects();
         ServerRequest_handleMapData(response);
         getALEIMapDataFromALEIMapDataObject(); //map data object >>> aleiMapData
         loadALEIMapDataIntoUse(); //aleiMapData >>> data in use
