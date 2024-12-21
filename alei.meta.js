e01cf87ff2e51bc2fd60951c5462524012d555ad
iatealemon <84639864+iatealemon@users.noreply.github.com>
2024-12-21 03:19:46 +0200

    Increment version and build

--- alei.meta.js
+++ alei.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name ALE Improvements
 // @description Changes to make ALE better.
-// @version 22.7
+// @version 22.8
 // @author molis, mici1234, wanted2001, gcp5o
 // @match *://www.plazmaburst2.com/level_editor/map_edit.php*
 // @connect github.com
