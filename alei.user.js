6dd875d777f07324308b2b79d34ef12113faf3e2
Molisson <molisson@tutamail.com>
2025-06-17 13:33:54 +0200

    Fix Tampermonkey script not loading on `plazmaburst2.com`
    (not `www.plazmaburst2.com`)

--- alei.user.js
+++ alei.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name ALE Improvements
 // @description Changes to make ALE better.
-// @version 26.1
+// @version 26.2
 // @author molis, mici1234, wanted2001, gcp5o
 // @match *://www.plazmaburst2.com/level_editor/map_edit.php*
+// @match *://plazmaburst2.com/level_editor/map_edit.php*
 // @connect github.com
 // @connect githubusercontent.com
 // @downloadURL https://github.com/Molisson/ALEI/raw/main/alei.user.js
